cfa76b1a0c548d3689bb881a8a01871e53d8302e
Utkarsh9721 <raiutkarsh544@gmail.com>
2025-06-28 13:23:46 +0530

    Update bookTable.jsx

--- src/components/bookTable/bookTable.jsx
+++ src/components/bookTable/bookTable.jsx
@@ -42,7 +42,7 @@ const handleSubmit = async (e) => {
   setIsSubmitting(true);
   
   try {
-    const response = await fetch('http://localhost:5000/api/bookings', {
+    const response = await fetch('https://hotel-1-dwls.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
