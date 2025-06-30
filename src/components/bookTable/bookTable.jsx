ffd236c4cdb2f6cf8cbae54e3053cc47abd999f9
Utkarsh9721 <raiutkarsh544@gmail.com>
2025-06-30 22:41:06 +0530

    Update bookTable.jsx

--- src/components/bookTable/bookTable.jsx
+++ src/components/bookTable/bookTable.jsx
@@ -42,7 +42,7 @@ const handleSubmit = async (e) => {
   setIsSubmitting(true);
   
   try {
-    const response = await fetch('https://backend-hotel-h18w-a5efnmw92-raiutkarsh544-gmailcoms-projects.vercel.app/api/bookings', {
+    const response = await fetch('https://backend-hotel-vnlc.onrender.com/api/bookings', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
